da1131d96f142a6cf729825e636befdcf53282f0
ttwkr <ttwkor12@gmail.com>
2020-11-18 12:43:16 +0900

    따라 다니는놈 위치 설정

--- Assets/Script/Follower.cs
+++ Assets/Script/Follower.cs
@@ -31,15 +31,28 @@ public class Follower : MonoBehaviour
 
     void Watch()
     {
-        // 큐에 넣는다
-        parentsPos.Enqueue(parents.position);
+        // 플레이어가 멈춰있을때 플레이어랑 겹치는거 방지
+        // 큐에 플레이어의 위치가 없으면 큐에 넣는다
+        if (!parentsPos.Contains(parents.position))
+        {
+            // 큐에 넣는다
+            parentsPos.Enqueue(parents.position);    
+        }
+        
         // 따라갈 위치를 계속 갱신
         if (parentsPos.Count > followDelay)
         {
             // 큐에 일정 데이터가 채워지면 그때부터 반환
-            followPos = parentsPos.Dequeue();    
+            followPos = parentsPos.Dequeue();
         }
-        
+        else if (parentsPos.Count < followDelay)
+        {
+            // 큐킈 수가 프레임(딜레이)보다 낮으면 플레이어의 위치
+            // 어디까지나 임시방편
+            // 처음 시작할때 위치를 초기화하기 위해
+            followPos = parents.position;
+        }
+
     }
 
     void Follow()
