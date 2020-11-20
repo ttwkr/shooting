eddb71bf124625842632613e231a31885a001423
ttwkr <ttwkor12@gmail.com>
2020-11-21 03:08:28 +0900

    fix bug

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -43,7 +43,7 @@ public class Boss : MonoBehaviour
 
         Rigidbody2D rigid = GetComponent<Rigidbody2D>();
         rigid.velocity = Vector2.zero;
-        // isDead = false;
+        isDead = false;
         Invoke("Think", 2);
     }
 
